e057f81860eee934624685c2cf6f86f6ee52d284
ronnathaniel <ronnathaniel>
2021-08-16 15:15:34 -0400

    fix(aws/index): simplified exported funcs

--- lib/contrib/aws-cdk/index.ts
+++ lib/contrib/aws-cdk/index.ts
@@ -1,39 +1,32 @@
 import { Construct } from '@aws-cdk/core';
 import {
-    EpsagonAwsCdkFunctionProps,
-    /* to be brought back in */
-    EpsagonAwsCdkSingletonFunctionProps
+    LambdaFunctionProps,
+    LambdaSingletonFunctionProps,
 } from './models';
 import { instrumentFunction } from './utils';
 import {
-    Function as AwsCdkFunction,
-    /* to be brought back in */
-    SingletonFunction as AwsCdkSingletonFunction,
-    FunctionProps as AwsCdkFunctionProps,
-    /* to be brought back in */
-    SingletonFunctionProps as AwsCdkSingletonFunctionProps,
+    Function,
+    SingletonFunction,
+    FunctionProps,
+    SingletonFunctionProps,
 } from '@aws-cdk/aws-lambda'
 
 
-export class EpsagonFunction extends Construct {
-
-  constructor(scope: Construct, id: string, props: EpsagonAwsCdkFunctionProps) {
+export class LambdaFunction extends Construct {
+  constructor(scope: Construct, id: string, props: LambdaFunctionProps) {
     super(scope, id);
-    props = instrumentFunction(props) as AwsCdkFunctionProps;
-    console.log("FINAL func props ::")
-    console.log(props)
-    new AwsCdkFunction(this, id, props);
+    new Function(
+        this, id, instrumentFunction(props) as FunctionProps,
+    );
   }
 }
 
 
-export class EpsagonSingletonFunction extends Construct {
-
-  constructor(scope: Construct, id: string, props: EpsagonAwsCdkSingletonFunctionProps) {
+export class LambdaSingletonFunction extends Construct {
+  constructor(scope: Construct, id: string, props: LambdaSingletonFunctionProps) {
     super(scope, id);
-    props = instrumentFunction(props) as AwsCdkSingletonFunctionProps;
-    console.log("FINAL func props ::")
-    console.log(props)
-    new AwsCdkSingletonFunction(this, id, props);
+    new SingletonFunction(
+        this, id, instrumentFunction(props) as SingletonFunctionProps,
+    );
   }
 }
