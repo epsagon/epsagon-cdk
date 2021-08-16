f6c378ed541f307df523b733c067e68204e7cc30
ronnathaniel <ronnathaniel>
2021-08-16 15:19:49 -0400

    fix(aws/models): unexported + commented the pre-released experimental functions

--- lib/contrib/aws-cdk/models.ts
+++ lib/contrib/aws-cdk/models.ts
@@ -3,22 +3,69 @@ import {
     FunctionProps as AwsFunctionProps,
     SingletonFunctionProps as AwsSingletonFunctionProps
 } from '@aws-cdk/aws-lambda';
+
 import { EpsagonConfig } from '../../models';
 
 /**
- * interface EpsagonAwsCdkFunctionProps
+ * interface LambdaFunctionProps
  * extends EpsagonConfig and AwsFunctionProps
  *  - the props passed to Epsagon-traced functions on creation
  */
-export interface EpsagonAwsCdkFunctionProps extends EpsagonConfig, AwsFunctionProps { }
+export interface LambdaFunctionProps extends
+    AwsFunctionProps, EpsagonConfig { }
 
 /**
- * interface EpsagonAwsCdkSingletonFunctionProps
+ * interface LambdaSingletonFunctionProps
  * extends EpsagonConfig and AwsSingletonFunctionProps
- *  - the props passed to Epsagon-traced singleton functions on creation
+ *  - the props passed to Epsagon-traced Singleton functions on creation
  */
-export interface EpsagonAwsCdkSingletonFunctionProps extends EpsagonConfig, AwsSingletonFunctionProps { }
+export interface LambdaSingletonFunctionProps extends
+    AwsSingletonFunctionProps, EpsagonConfig { }
 
 
-export type AnyAwsCdkFunctionProps = AwsFunctionProps | AwsSingletonFunctionProps;
-export type AnyEpsagonAwsCdkFunctionProps = EpsagonAwsCdkFunctionProps | EpsagonAwsCdkSingletonFunctionProps;
+
+/**
+ * AnyFunctionProps
+ * Represents Any @aws-cdk/aws-lambda Function Props. Both Normal & Singleton.
+ */
+export type AnyFunctionProps = AwsFunctionProps | AwsSingletonFunctionProps;
+
+
+/**
+ * AnyLambdaFunctionProps
+ * Represents any Epsagon-extending interface for @aws-cdk/aws-lambda Function Props. Both Normal & Singleton.
+ */
+export type AnyLambdaFunctionProps = LambdaFunctionProps | LambdaSingletonFunctionProps;
+
+
+/*
+ *  These runtime-specific function props were purposely removed from the original release
+ *  Since typescript does not allow for unused declarations, they were commented out for the time being.
+ *  When the time will come to support these experimental functions - they can simply be uncommented.
+ */
+
+
+// import { PythonFunctionProps } from '@aws-cdk/aws-lambda-python';
+// import { NodejsFunctionProps } from '@aws-cdk/aws-lambda-nodejs';
+/**
+ * interface LambdaPythonFunctionProps
+ * extends EpsagonConfig and PythonFunctionProps
+ *  - the props passed to Epsagon-traced Python functions on creation
+ */
+// export interface LambdaPythonFunctionProps extends
+    // PythonFunctionProps, EpsagonConfig { }
+
+/**
+ * interface LambdaNodejsFunctionProps
+ * extends EpsagonConfig and NodejsFunctionProps
+ *  - the props passed to Epsagon-traced Nodejs functions on creation
+ */
+// export interface LambdaNodejsFunctionProps extends
+//     NodejsFunctionProps, EpsagonConfig { }
+
+// export type AnyLambdaRuntimeFunctionProps = LambdaPythonFunctionProps | LambdaNodejsFunctionProps;
+/**
+ * AnyRuntimeFunctionProps
+ * Represents any runtime-specific @aws-cdk/aws-lambda-LANG Function Props.
+ */
+// export type AnyRuntimeFunctionProps = PythonFunctionProps | NodejsFunctionProps
