b1fa1fc59d028bbe46fc3fc5175cf3b5dacbc19c
ronnathaniel <ronnathaniel>
2021-08-16 15:21:45 -0400

    fix(aws/utils): added bundling

--- lib/contrib/aws-cdk/utils.ts
+++ lib/contrib/aws-cdk/utils.ts
@@ -3,86 +3,78 @@
 import {
     EpsagonConfig, ObjectKeys, Mut, Immut,
 } from '../../models';
-import { AnyAwsCdkFunctionProps, AnyEpsagonAwsCdkFunctionProps } from './models';
+import { AnyFunctionProps, AnyLambdaFunctionProps } from './models';
 import { wrapper, deconstructHandler } from '../../handlers';
 import { EPSAGON_HANDLERS_DIR } from '../../const';
 import {
-    Code, InlineCode,
-    S3Code, AssetCode,
+    Code,
+    InlineCode,
+    S3Code,
+    AssetCode,
 } from '@aws-cdk/aws-lambda';
-import { promisify } from 'util';
-
 import * as fs from 'fs';
-import { join as pathJoin } from 'path';
+import {
+    join as pathJoin,
+    resolve as pathResolve
+} from 'path';
 
-
-const writeFile = promisify(fs.writeFile);
-const mv = promisify(fs.rename);
 const mkdir = (handlerPath: string) => {
     try {
         fs.mkdirSync(handlerPath);
     } catch (err) {
-        // if (err instanceof )
-        console.error(err.message)
+        console.error('Error creating dir')
+        console.error(err.message);
     }
 }
 
-// const rm = (dirPath: string) =>
-//     fs.rmdir(dirPath, (err) => {
-//         if (err) {
-//             console.warn('stam error:')
-//             console.warn(err)
-//         }
-//     });
+const pipeToFile = (filePath: string, content: string) => {
+    try {
+        fs.writeFileSync(filePath, content);
+    } catch (err) {
+        console.error('Error writing to path');
+        console.error(err.message);
+    }
+}
 
+const resolveHandlersDir = (dirPath: string | undefined) =>
+    pathResolve(
+        pathJoin(dirPath || '', EPSAGON_HANDLERS_DIR)
+    );
 
 const writeHandler = (
     {funcInfo, fileConf}:
         {
-            funcInfo: AnyAwsCdkFunctionProps,
+            funcInfo: AnyFunctionProps,
             fileConf: {
                 fileExt: string,
-                wrapperCode: string
+                wrapperCode: string,
+                path?: string,
             }
         }
 ) => {
-    mkdir(EPSAGON_HANDLERS_DIR)
-    console.log('functioninfo:: + name::, ', funcInfo)
+    const handlersDir = resolveHandlersDir(fileConf.path);
+    mkdir(handlersDir)
     const funcName = funcInfo.functionName || 'main';
     const handlerPath = pathJoin(
-        EPSAGON_HANDLERS_DIR,
+        handlersDir,
         funcName,
     )
-    Promise.resolve(
-        writeFile(
-            `${handlerPath}.${fileConf.fileExt}`,
-            fileConf.wrapperCode,
-        )
-    )
-        .then(r => r)
-        .catch(err => console.error(err));
-    console.log('wrote to ', pathJoin(
-                EPSAGON_HANDLERS_DIR,
-                `${funcName}.${fileConf.fileExt}`,
-            ))
-    console.log('this is what i just wrote:::')
-    console.log(fileConf.wrapperCode);
-    console.log('the handler:::::::::')
-    console.log(funcInfo.handler)
-
+    pipeToFile(`${handlerPath}.${fileConf.fileExt}`, fileConf.wrapperCode);
     const { methodName:  handlerMethodName} = deconstructHandler(funcInfo.handler);
-    return `${funcName}.${handlerMethodName}`
+    return `${fileConf.path ? pathJoin(EPSAGON_HANDLERS_DIR, funcName) : funcName}.${handlerMethodName}`
 }
 
-// function cleanUp() {
-//     rm(EPSAGON_HANDLERS_DIR);
-// }
 
-
-
-export function createEpsagonConf(props: AnyEpsagonAwsCdkFunctionProps): EpsagonConfig {
+export function createEpsagonConf(props: AnyLambdaFunctionProps): EpsagonConfig {
     let epsagonConf: ObjectKeys = {};
-    // Object.keys(props as EpsagonConfig).map(e => console.log(e));
+    /*
+     * typescript is not mature enough to collect keys of an interface
+     * without first instantiating it as a class/object. This feature will one
+     * day be added to typescript, at which point, we can remove this hardcoded list.
+     *
+     * Until that day - this is the ultimate source of truth as to which
+     * epsagon configuration keys are collected.
+     */
     ([
         'token',
         'appName',
@@ -96,98 +88,52 @@ export function createEpsagonConf(props: AnyEpsagonAwsCdkFunctionProps): Epsagon
             delete props[p];
         });
 
-    console.log('props')
-    console.log(props)
-
-    console.log('props.environment');
-    console.log(props.environment, typeof props.environment);
-
-    console.log('props.function-handler');
-    console.log(props.handler, typeof props.handler);
-
-    console.log('handmade epsagonConf:')
-    console.log(epsagonConf)
-
-
-
     return epsagonConf as EpsagonConfig;
 }
 
-export function instrumentFunction(funcProps: AnyEpsagonAwsCdkFunctionProps): AnyAwsCdkFunctionProps {
+export function instrumentFunction(funcProps: AnyLambdaFunctionProps): AnyFunctionProps {
 
-    // const mutProps: Mut = props;
-
-    // const { handler } = funcProps;
-    // console.log(handler)
     const epsagonConf = createEpsagonConf(funcProps);
     const funcPropsMut: Mut<typeof funcProps> = funcProps;
-    // const funcPropsMut = (funcProps as AnyEpsagonAwsCdkFunctionProps) as Mut<AnyEpsagonAwsCdkFunctionProps>;
-
-    // const wrapperCode: string = wrapper(funcProps, epsagonConf);
     const { code: codeOriginal } = funcPropsMut
-
-        // .then(r => {
-        //     console.log('BEFORE')
-        //     console.log('return:', r)
-        //     funcPropsMut.handler = r
-        //     console.log('AFTER')
-        //     console.log('new handler: ', funcPropsMut.handler)
-        //
-        // })
-        // .then(r => r)
-        // .catch(err => console.error(err))
-
-    let codeWrapped;
-    console.log('got:', codeOriginal)
-    console.log('constructor:', codeOriginal.constructor)
-    console.log('inline:', InlineCode)
-
-    // if (codeOriginal.constructor == InlineCode) {
-
-
-    console.log(codeOriginal.constructor.name, InlineCode.name)
-    console.log(codeOriginal.constructor.name == InlineCode.name)
-    console.log(codeOriginal.constructor.name === InlineCode.name)
 
     switch (codeOriginal.constructor.name) {
         case InlineCode.name:
-
-            console.log('THIS IS INLINE CODE')
-            console.log('==========================================')
-            const { wrapperCode, fileExt } = wrapper(funcPropsMut, epsagonConf, (codeOriginal as any).code);
-
-            console.log('original code:::', codeOriginal, typeof codeOriginal)
-            console.log(codeOriginal)
-            console.log('wrapped code=====', wrapperCode, typeof codeWrapped)
-            console.log(codeWrapped)
-            console.log('=============')
-
-            // funcPropsMut.code = new InlineCode(codeWrapped);
-            // funcPropsMut.code = Code.fromInline(wrapperCode);
-            funcPropsMut.handler = writeHandler({funcInfo: funcPropsMut as Mut<AnyAwsCdkFunctionProps>, fileConf: {fileExt, wrapperCode}})
-            funcPropsMut.code = Code.fromAsset(EPSAGON_HANDLERS_DIR)
-            console.log('mutable code:::')
-            console.log(funcPropsMut.code)
-            break;
-    // }
-        case S3Code.name:
         case AssetCode.name:
-            console.log('NOT SUPPORTED')
-            codeWrapped = 'not empty...';
+            const {
+                wrapperCode,
+                fileExt,
+                bundleOpts,
+            } = wrapper(funcPropsMut, epsagonConf, (codeOriginal as any).code);
+            const assetPath = (codeOriginal as any).path || EPSAGON_HANDLERS_DIR;
+            console.log({assetPath})
+            funcPropsMut.handler = writeHandler({
+                funcInfo: funcPropsMut as Mut<AnyFunctionProps>,
+                fileConf: {fileExt, wrapperCode, path: (codeOriginal as any).path}
+            });
+            funcPropsMut.code = Code.fromAsset(assetPath, {
+                bundling: {
+                    command: [
+                        'bash', '-c', bundleOpts,
+                    ],
+                    image: funcPropsMut.runtime.bundlingImage,
+                    user: 'root'
+                },
+            });
             break;
+        case S3Code.name:
+            /* TODO
+             * until added, fall through to default
+             * */
         default:
-            console.log('DEFAULTED')
-            console.log('codeOriginal.constructor', codeOriginal.constructor)
-            console.log('codeOriginal.constructor.name', codeOriginal.constructor.name)
-            codeWrapped = '';
+            /*
+             * In case of an unsupported Code platform:
+             * No instrumentation is added to the function
+             * The function will work as-is, untouched by Epsagon.
+             */
             break;
-
-
     }
 
-
-
-
-    return funcPropsMut as Immut<AnyAwsCdkFunctionProps>;
+    return funcPropsMut as Immut<typeof funcProps>;
 }
 
