308839e893a40d4128b7e5b8134496161748d25d
ronnathaniel <ronnathaniel>
2021-08-16 15:23:42 -0400

    fix(models): defined mut + immut

--- lib/models.ts
+++ lib/models.ts
@@ -20,13 +20,20 @@ export interface EpsagonConfig {
  * :param T: type of initial object pre-mutation
  * :return Mut<T>: mutable copy of the same object T
  */
-export type Mut<T> = { -readonly [key in keyof T]: T[key] }
+export type Mut<T> = { -readonly [key in keyof T]: T[key] };
+
 /**
- *
+ * Immut<T>: immutable
+ * turns all fields of T to immutable
+ * :param T: type of initial object pre-mutation
+ * :return Immut<T>: immutable copy of the same object T
  */
-export type Immut<T> = { +readonly [key in keyof T]: T[key] }
+export type Immut<T> = { +readonly [key in keyof T]: T[key] };
 
-
+/**
+ * ObjectKeys
+ * represents a key-value mapping between strings to most values
+ */
 export interface ObjectKeys {
     [key: string]: string | boolean | object | undefined
-}
+};
