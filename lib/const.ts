7167dfdda98ff46bcde4ada1e7d19e360cd48106
ronnathaniel <ronnathaniel>
2021-09-13 17:01:17 -0400

    feat(const): formatting

--- lib/const.ts
+++ lib/const.ts
@@ -3,4 +3,8 @@ import { join as pathJoin } from 'path';
 
 
 
-export const EPSAGON_HANDLERS_DIR: string = pathJoin('.', 'epsagon_handlers') || pathJoin(__dirname, 'epsagon_handlers');
+export const EPSAGON_HANDLERS_DIR: string = (
+    pathJoin('.', 'epsagon_handlers')
+    ||
+    pathJoin(__dirname, 'epsagon_handlers')
+);
